e17c3dee5fd8d665057ed32d50e4edcbb3a18ca0
Haque <Shabab.Haque8@T-Mobile.com>
2020-06-25 18:12:15 -0500

    everything except for modals

--- testSite.js
+++ testSite.js
@@ -1,31 +1,35 @@
-$(document).ready(function(){
-    characterList();
-    specificChar();
+$(document).ready(function () {
+  characterList();
+  specificChar();
 });
 
 function characterList() {
-    var settings = {
-        "url": "https://www.breakingbadapi.com/api/characters",
-        "method": "GET",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
-        //console.log(response);
+  var settings = {
+    "url": "https://www.breakingbadapi.com/api/characters",
+    "method": "GET",
+    "timeout": 0,
+  };
 
-        
-        var characters = response;
-        characters.forEach(el => {
-          var element = '<div class="col m4 s12" style="border: 5px solid transparent;" id="'+el.char_id+'">';
-          element+= '<a href="#charModal" class="modal-trigger"><div class="card-panel filter z-depth-4" style="height: 300px; border-radius: 10px; background-image: url(' + el.img + '); background-size: cover; backdrop-filter: blur(50px);"><section style="backdrop-filter: blur(10px);"><h5 class="white-text text-darken-2" style="font-weight: 700;">'+el.name+'</h5>';
-          element+= '<p class="black-text">Birthday: '+ el.birthday;
-          element+= '<br>Occupation: '+ el.occupation[0];
-          element+= '<br>Status: '+ el.status+'</p></section></div></a></div>';
-          $("#list").append(element);
-          
-        });
+  $.ajax(settings).done(function (response) {
+    //console.log(response);
 
-      });
+
+    var characters = response;
+    characters.forEach(el => {
+      var element = '<div class="col m4 s12" style="border: 5px solid transparent;" id="' + el.char_id + '">';
+      element += '<a href="#charModal" class="modal-trigger"><div class="card-panel filter z-depth-4" style="height: 300px; border-radius: 10px; background-image: url(' + el.img + '); background-size: cover; backdrop-filter: blur(50px);"><section style="backdrop-filter: blur(10px); top:70px"><h5 class="white-text text-darken-2" style="font-weight: 700">' + el.name + '</h5></section></div></a></div>';
+      $("#list").append(element);
+
+      // modal time
+
+      /*
+      var modalElement = '<div id="modal'+el.char_id+'" class="modal z-depth-4" style="border-radius: 15px;"><div class="modal-content"><h4>'+el.name+'</h4><p>A bunch of text</p></div></div>'
+
+      $('#modalList').append(modalElement);*/
+
+    });
+
+  });
 }
 
 function specificChar() {
@@ -34,34 +38,34 @@ function specificChar() {
   });*/
 
   let charID = null
-  $("#list").on('click', '.col', function(event) {
+  $("#list").on('click', '.col', function (event) {
     charID = $(this).attr('id');
     console.log(charID);
 
-    
-    var settings = {
-    "url": "https://www.breakingbadapi.com/api/characters/" + charID,
-    "method": "GET",
-    "timeout": 0,
-  };
-  
-  $.ajax(settings).done(function (response) {
-    //console.log(response);
-    let character = response;
 
-    
-    var element = '<div><h5>'+character[0].name+'</h5>';
-    element+= '<img src="'+character[0].img+'">'
-    element+= '<p>Birthday: '+character[0].birthday;
-    element+= '<br>Occupation: '+character[0].occupation;
-    element+= '<br>Nickname: '+character[0].nickname;
-    element+= '<br>Status: '+character[0].status+'</p></div>'; 
-    //console.log(element);
-    $('#singleChar').append(element);
-    
-  
+    var settings = {
+      "url": "https://www.breakingbadapi.com/api/characters/" + charID,
+      "method": "GET",
+      "timeout": 0,
+    };
+
+    $.ajax(settings).done(function (response) {
+      //console.log(response);
+      let character = response;
+
+
+      var element = '<div><h5>' + character[0].name + '</h5>';
+      element += '<img src="' + character[0].img + '">'
+      element += '<p>Birthday: ' + character[0].birthday;
+      element += '<br>Occupation: ' + character[0].occupation;
+      element += '<br>Nickname: ' + character[0].nickname;
+      element += '<br>Status: ' + character[0].status + '</p></div>';
+      //console.log(element);
+      $('#singleChar').append(element);
+
+
+    });
+
   });
-  
-});
 
 }
